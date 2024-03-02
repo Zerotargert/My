b6f352cc526a99746f603b1a345a1d27e719ad3b
Zerotargert <161900174+Zerotargert@users.noreply.github.com>
2024-03-02 03:43:54 +0300

    Me
    You

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" type="image/x-icon" href="img/icon.png">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9371976363197718"
+     crossorigin="anonymous"></script>
+  <title>wellcome</title>
+</head>
+<body>
+
+<header class="header">
+  <a class="logo">WELLCOME </a>
+  
+
+  <input type="checkbox" name="" id="check">
+  <label for="check" class="icon">
+    <span class="material-symbols-outlined">
+<img src="img/menu.svg" alt="">
+</span>
+  </label>
+  
+  
+  <nav class="navbar">
+    <a href="log.php" style="--i:0;">Home</a>
+    <a href="log.php" style="--i:1;">Qr Code generate</a>
+     <a href="log.php" style="--i:2;">Contact Us</a>
+    <a href="log.php" style="--i:3;">My Profile</a>
+  </nav>
+</header>
+
+<div class="bod">
+  <p>Wellcome to Template in the world</p><br>
+  <a href="log.php">Log In</a>
+  <a href="tel:0716513254">Call</a>
+</div>
+
+
+  <div class="end">
+    <p>©2024 template on the world, all right reasons</p>
+  </div>
+  
+  <script src="https://unpkg.com/scrollreveal"></script>
+<script src="main.js"></script>
+</body>
+</html>
