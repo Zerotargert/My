8abbef715fe5198709a3d2058cefe8e230923abd
Zerotargert <161900174+Zerotargert@users.noreply.github.com>
2024-03-02 03:46:31 +0300

    Me
    You

---
+++ main.js
@@ -0,0 +1,10 @@
+console.log( "Hello World!" );
+
+ScrollReveal({
+ reset: true,
+ distance: '80px',
+ duration: 1500,
+ delay: 200
+ });
+ 
+ ScrollReveal().reveal('.form, .sms,  .bod, .end p, .header a', { origin: 'top'});
